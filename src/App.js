dcc45e4bb86f52978bbcff3e671c1a45a4f0d20e
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-07 11:22:12 +0100

    Add useState, useEffect, fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Article from './components/Article';
-import { newsList } from './newsList'
+/* import { newsList } from './newsList' */
 
 function App() {
+  const [newsList, setNewsList] = useState([])
+
+  function  fetchNewsList() {
+    fetch('https://image-mock-data.firebaseio.com/articles.json')
+    .then( response => response.json(response) )
+    .then ( data => setNewsList(data) )
+  }
+  useEffect( () => {
+    fetchNewsList()
+  }, []
+  )
+
   return (
     <div className="container-fluid">
       <div className="row">
